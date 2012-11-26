7a14835776b3cc54e8b00ded205dc5d02685c5b7
Liraz Siri <liraz@turnkeylinux.org>
2012-11-26 21:40:02 +0200

    cli stub

--- parun.py
+++ parun.py
@@ -1,30 +1,56 @@
 #!/usr/bin/python
 # 
-# Copyright (c) 2013 Liraz Siri <liraz@turnkeylinux.org>
+# Copyright (c) 2012 Liraz Siri <liraz@turnkeylinux.org>
 # 
-# This file is part of Example.
+# This file is part of Parun.
 # 
-# Example is open source software; you can redistribute it and/or
+# Parun is open source software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 3 of
 # the License, or (at your option) any later version.
 # 
-"""Example command template
+"""Runs commands in parallel in a screen session
 
 Options:
-  -q --quiet	don't print anything
-  -f=FOO	print FOO
-  --fatal       fatal error
+    --name=STR            Name of screen session
+    --daemon
+
+    --minheight=LINES     Minimum screen window height (default: 8)
+                        
+                          If splitting the screen would produce windows shorter
+                          than this show a window list instead.
+
+                          Environment variable: PARUN_MINHEIGHT
+
+Example usage:
+
+    # adhoc command line
+    cat /etc/passwd | parun "echo hello; echo =====; echo; cat; sleep 10" \\
+                            "echo world; echo =====; echo; cat; sleep 10"
+
+    # as an executable
+    cat > example << 'EOF'
+    #!/usr/bin/env parun
+    echo hello; echo =====; echo; cat; sleep 10
+    echo world; echo =====; echo; cat; sleep 10
+    EOF
+
+    chmod +x ./example
+    cat /etc/passwd | ./example
 
 """
+import os
 import sys
 import getopt
+
+DEFAULT_MINHEIGHT = 8
 
 def usage(e=None):
     if e:
         print >> sys.stderr, "error: " + str(e)
 
-    print >> sys.stderr, "Syntax: %s [args]" % sys.argv[0]
+    print >> sys.stderr, "Syntax: %s [ -options ] 'cmd1 args' 'cmd2 args' ... " % sys.argv[0]
+    print >> sys.stderr, "Syntax: %s [ -options ] path/to/command-list" % sys.argv[0]
     print >> sys.stderr, __doc__.strip()
     sys.exit(1)
 
@@ -34,27 +60,34 @@ def fatal(s):
 
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'qf:h', ['fatal'])
+        opts, args = getopt.gnu_getopt(sys.argv[1:], 'h', ['name=',
+                                                           'minheight=',
+                                                           'daemon'])
     except getopt.GetoptError, e:
         usage(e)
 
-    opt_quiet = False
+    if not args:
+        usage()
+
+    session_name = None
+    daemonize = False
+    minheight = int(os.environ.get('PARUN_MINHEIGHT', str(DEFAULT_MINHEIGHT)))
+
     for opt, val in opts:
         if opt == '-h':
             usage()
 
-        if opt in ('-q', '--quiet'):
-            opt_quiet = True
+        if opt == '--daemon':
+            daemonize = True
 
-        elif opt == '-f':
-            print "printing foo: " + val
+        if opt == '--minheight':
+            minheight = int(val)
 
-        elif opt == '--fatal':
-            fatal("fatal condition")
+        if opt == '--name':
+            session_name = val
 
-    if not opt_quiet:
-        print "printing args: " + `args`
-
+    for var in ('session_name', 'daemonize', 'minheight', 'args'):
+        print "%s = %s" % (var, `locals()[var]`)
     
 if __name__=="__main__":
     main()
